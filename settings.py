d3c556c05c23482fb925b7d58a5aad6bff26b96e
marvij7823-stack <marvij7823@gmail.com>
2026-03-01 10:50:04 +0200

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,14 @@
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 400
+WHITE = (255, 255, 255)
+BLACK = (0,0,0)
+GREY = (200, 200, 200)
+KEYS = {
+"a": "w.mp3",
+"b": "ww.mp3",
+"d": "www.mp3",
+"f": "e.mp3",
+"g": "q.mp3",
+"e": "qq.mp3",
+"c": "qqq.mp3"
+}
